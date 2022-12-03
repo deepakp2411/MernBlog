a62665f68cfb3342cfec0432fdb2d5634757d715
Deepak <bbaz2deepak@gmail.com>
2022-12-03 12:31:50 +0530

    first commit

--- backend/controller/users.js
+++ backend/controller/users.js
@@ -1,12 +1,58 @@
 import UserModel from "../models/userModel.js";
 
 export const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserModel.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await Promise.all(
+      user.friends.map((id) => UserModel.findById(id))
+    );
+    const formattedFriends = friends.map({
+      _id,
+      firstName,
+      lastName,
+      occupation,
+      location,
+      picturePath,
+    });
+
+    res.status(200).json(formattedFriends);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+// Update //
+
+export const addRemoveFriend = async (req,res) => {
     try {
-        const { id } = req.params;
-        const user = await UserModel.findById(id)
-        res.status(200).json(user);
+        
+        const { id, friendId} = req.params;
+        const user = await UserModel.findById(id);
+        const friend = await UserModel.findById(friendId)
+
+        if(user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id)
+
+        }else{
+            user.friends.push(friendId);
+            friend.friends.push(id)
+        }
+        await user.save()
+        await friend.save()
         
     } catch (error) {
-        res.status(404).json({ message: error.message})
+        res.status(404).json({ message: error.message });
     }
 }
